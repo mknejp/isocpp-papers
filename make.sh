e344f7786b06bb434903b75bde975b85cef6bdda
Miro Knejp <miro.knejp@gmail.com>
2017-01-17 07:43:09 +0100

    syntax highlighting with pygments

--- make.sh
+++ make.sh
@@ -3,5 +3,5 @@ export LANG=en_US.UTF-8
 
 mkdir -p docs
 for file in $(basename -s .rst *.rst); do
-	./rst2html.py $file.rst docs/$file.html
+	./rst2html.py --syntax-highlight=short --stylesheet=style.css,pygments.css $file.rst docs/$file.html
 done
