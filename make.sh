2835100bd96630877067d924c65460a926d20752
Miro Knejp <miro.knejp@gmail.com>
2017-01-17 07:42:51 +0100

    autodetect all .rst files

--- make.sh
+++ make.sh
@@ -1,9 +1,7 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-FILES=(dxxxx-allocate_unique dxxxx-deduction-guides)
-
 mkdir -p docs
-for file in dxxxx-allocate_unique dxxxx-deduction-guides; do
+for file in $(basename -s .rst *.rst); do
 	./rst2html.py $file.rst docs/$file.html
 done
